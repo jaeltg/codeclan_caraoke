aac40a1407830db6c682fa7f669226c844230d19
jaeltg <jaeltg@hotmail.com>
2021-03-05 17:46:37 +0000

    added tests for guest has name and guest has wallet

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -6,5 +6,13 @@ from classes.song import Song
 class TestGuest(unittest.TestCase):
    
     def setUp(self):
-        pass
+        self.song = Song("La Bilirrubina", "Juan Luis Guerra")
+        self.guest_1 = Guest("Andrea", 200)
+        self.guest_2 = Guest("Gabriel", 100)
+        
+    def test_guest_has_name(self):
+        self.assertEqual("Andrea", self.guest_1.name)
+
+    def test_guest_has_wallet(self):
+        self.assertEqual(100, self.guest_2.wallet)        
 
